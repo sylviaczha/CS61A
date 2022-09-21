1451bab8b0840e9f9ad7a058b0a3f1eb58ddc434
Sylvia Zhang <54140043+sylviaczha@users.noreply.github.com>
2022-09-21 19:49:25 +0800

    Update hw02.recursion.py

--- homework/hw02.recursion.py
+++ homework/hw02.recursion.py
@@ -88,3 +88,108 @@ def pingpong2(n):
 # a helper function is helpful when having extra values to keep track of
 # a helper function is the mirror image of what the while loop is doing
 # each recursive framework is going to be a single while loop
+
+
+def missing_digits(n):
+    """Given a number a that is in sorted, increasing order,
+    return the number of missing digits in n. A missing digit is
+    a number between the first and last digit of a that is not in n.
+    >>> missing_digits(1248) # 3, 5, 6, 7
+    4
+    >>> missing_digits(1122) # No missing numbers
+    0
+    >>> missing_digits(123456) # No missing numbers
+    0
+    >>> missing_digits(3558) # 4, 6, 7
+    3
+    >>> missing_digits(35578) # 4, 6
+    2
+    >>> missing_digits(12456) # 3
+    1
+    >>> missing_digits(16789) # 2, 3, 4, 5
+    4
+    >>> missing_digits(19) # 2, 3, 4, 5, 6, 7, 8
+    7
+    >>> missing_digits(4) # No missing numbers between 4 and 4
+    0
+    >>> from construct_check import check
+    >>> # ban while or for loops
+    >>> check(HW_SOURCE_FILE, 'missing_digits', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n < 10:
+        return 0
+    elif (n % 10 - (n // 10) % 10) == 0:
+        return (n % 10 - (n // 10) % 10) + missing_digits(n // 10)
+    else:
+        return (n % 10 - (n // 10) % 10 - 1) + missing_digits(n // 10)
+
+
+    def next_largest_coin(coin):
+    """Return the next coin.
+    >>> next_largest_coin(1)
+    5
+    >>> next_largest_coin(5)
+    10
+    >>> next_largest_coin(10)
+    25
+    >>> next_largest_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+
+def next_smallest_coin(coin):
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+
+def start_coin(coin):
+    if coin >= 25:
+        return 25
+    elif coin >= 10:
+        return 10
+    elif coin >= 5:
+        return 5
+    else:
+        return 1
+
+
+def count_coins(total):
+    """Return the number of ways to make change for total using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+    def helper(total, n):
+        if total < 0:
+            return 0
+        elif total == 0:
+            return 1
+        elif n == 1:
+            return 1
+        else:
+            return helper(total - n, n) + helper(total, next_smallest_coin(n))
+    return helper(total, start_coin(total))
+
+
