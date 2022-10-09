98dfe7f35aef4520257d07a7337e4bf3bb4d8960
Sylvia Zhang <54140043+sylviaczha@users.noreply.github.com>
2022-10-09 17:22:30 +0800

    Update Lecture15 Mutable Values.py

--- lecture/Lecture15 Mutable Values.py
+++ lecture/Lecture15 Mutable Values.py
@@ -64,7 +64,7 @@ def dict_demos():
     numerals['I'] = 1
     numerals['L'] = 50
     numerals
-    sum(numerals.value())
+    sum(numerals.values())
     dict([(3, 9), (4, 16), (5, 25)])
     numerals.get('A', 0)
     numerals.get('V', 0)
