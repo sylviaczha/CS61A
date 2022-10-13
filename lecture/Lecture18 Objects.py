eeba2e51413b1b3ee5b21bdde1b4276950f0f3e8
Sylvia Zhang <54140043+sylviaczha@users.noreply.github.com>
2022-10-13 10:28:21 +0800

    Create Lecture18 Objects.py

---
+++ lecture/Lecture18 Objects.py
@@ -0,0 +1,23 @@
+class Clown:
+    nose = 'big and red'
+    def dance():
+        return 'No thanks'
+
+class Account:
+    interest = 0.02
+
+    def __init__(self, account_holder):
+        self.holder = account_holder
+        self.balance = 0
+
+    def deposit(self, amount):
+        """Add amount to balance."""
+        self.balance = self.balance + amount
+        return self.balance
+
+    def withdraw(self, amount):
+        """Subtract amount from balance if funds are available."""
+        if amount > self.balance:
+            return 'Insufficient funds'
+        self.balance = self.balance - amount
+        return self.balance
