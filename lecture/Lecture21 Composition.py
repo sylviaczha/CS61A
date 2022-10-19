615e0c0523798c1cb6756618df293dd619af1b1e
Sylvia Zhang <54140043+sylviaczha@users.noreply.github.com>
2022-10-19 16:30:17 +0800

    Create Lecture21 Composition.py

---
+++ lecture/Lecture21 Composition.py
@@ -0,0 +1,150 @@
+class Link:
+    empty = ()
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+def square(x):
+    return x * x
+
+def odd(x):
+    return x % 2 == 1
+
+def range_link(start, end):
+    """Return a Link containing consecutive integers from start to end."""
+    if start >= end:
+        return Link.empty
+    else:
+        return Link(start, range_link(start + 1, end))
+
+def map_link(f, s):
+    """Return a Link containing f(x) for each x in Link s."""
+    if s is Link.empty:
+        return s
+    else:
+        return Link(f(s.first), map_link(f, s.rest))
+
+def filter_link(f, s):
+    """Return a Link containing only the elements x of Link s for which f(x) is a true value."""
+    if s is Link.empty:
+        return s
+    if f(s.first):
+        return Link(s.first, filter_link(f, s.rest))
+    else:
+        return filter_link(f, s.rest)
+
+r = range_link(1, 6)
+s = filter_link(odd, r)
+t = map_link(square, s)
+
+
+#adding a set represented as an ordered list
+def add(s,v):
+    assert s is not List.empty
+    if s.first > v:
+        s.first, s.rest = v, Link(s.first, s.rest)
+    elif s.first < v and empty(s.rest):
+        s.rest = Link(v)
+    elif s.first < v:
+        add(s.rest, v)
+    return s
+
+
+#tree class
+class Tree:
+    def __init__(self, label, branches=[]):
+        self.label = label
+        for branch in branches:
+            assert isinstance(branch, Tree)
+        self.branches = list(branches)
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            banche_str = ''
+        return 'Tree({0}{1})'.format(repr(self.label), branch_str)
+
+    def __str__(self):
+        return '\n'.join(self.indented())
+
+    def indented(self):
+        lines = []
+        for line in self.branches:
+            lines.append('  ' + line)
+        return [str(self.label)] + lines
+
+    def is_leaf(self):
+        return not self.branches
+    
+
+def fib_tree(n):
+    if n == 0 or n == 1:
+        return Tree(n)
+    else:
+        left = fib_tree(n-2)
+        right = fib_tree(n-1)
+        fib_n = left.label + right.label
+        return Tree(fib_n, [left, right])
+
+def leaves(t):
+    """Return a list of leaf lables in Tree T."""
+    if t.is_leaf():
+        return [t.label]
+    else:
+        all_leaves = []
+        for b in t.branches:
+            all_leaves.extend(leaves(b))
+        return all_leaves
+
+def height(t):
+    """Return the number of transitions in the longest path in T."""
+    if t.is_leaf():
+        return 0
+    else:
+        return 1 + max([height(b) for b in t.branches])
+
+
+#tree data abstraction
+def tree(label, branches=[]):
+    for branch in branches:
+        assert is_tree(branch)
+    return [label] + list(branches)
+
+def label(tree):
+    return tree[0]
+
+def branches(tree):
+    return tree[1:]
+
+def fib_tree(n):
+    if n == 0 or n == 1:
+        return tree(n)
+    else:
+        left = fib_tree(n-2)
+        right = fib_tree(n-1)
+        fib_n = label(left) + label(right)
+        return tree(fib_n, [left, right])
+
+
+#tree mutation
+def prune(t, n):
+    """prune all sub-trees whose label is n."""
+    t.branches = [b for b in t.branches if b.label != n]
+    for b in t.branches:
+        prune(b, n)
